2f97c3540631153d93fa65c81a6544484741b6f9
pfczx <marcel.ras@edu.uekat.pl>
2024-12-21 17:21:20 +0100

    chore: added setup, updated requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = os.getenv(
+    "VERSION", "0.0.0"
+)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+
+
+
+setup(
+    name="SON_TESTY",
+    version=version,
+    description="Terrible code",
+    url="https://github.com/pfczx/SON_TESTY",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
